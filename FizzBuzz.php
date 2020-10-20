d250992e6565b87a3bd4cc45fab4c876647605ae
sewelseb <sewelseb@hotmail.com>
2020-10-20 17:38:00 +0200

    refactor - manage multiple of 3 with a modula

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,7 +6,7 @@ class FizzBuzz
 
     public function count(int $int): string
     {
-        if($int === 3 || $int ===6) return "Fizz";
+        if($int%3 === 0) return "Fizz";
         if($int === 5) return "Buzz";
 
         return  strval($int);
